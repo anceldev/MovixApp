a7e2a72f3c345855644d679dcb0ede336c1cc771
Ancel Guarayo <aguarayo@gmail.com>
2024-07-13 02:51:14 +0200

    First commit

---
+++ MovixApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MovixApp
+//
+//  Created by Ancel Dev account on 13/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MovixApp/MovixAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovixAppApp.swift
+//  MovixApp
+//
+//  Created by Ancel Dev account on 13/7/24.
+//
+
+import SwiftUI
+
+@main
+struct MovixAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
